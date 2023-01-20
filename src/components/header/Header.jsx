f84233601a368394d09dbed93f2d781d257552df
FranzKhyl <117539969+FranzKhylOng@users.noreply.github.com>
2023-01-21 02:58:29 +0800

    test deploy

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,21 +3,23 @@ import './header.css'
 import CTA from './CTA'
 import ME from '../../assets/me.png'
 import HeaderSocial from './HeaderSocials'
+import Zoom from 'react-reveal/Zoom';
+
 
 const Header = () => {
   return (
     <header>
       <div className="container header__container">
         <h5>Hello I'm</h5>
-        <h1>Franz Khyl Ong</h1>
+        <Zoom>
+          <h1>Franz Khyl Ong</h1>
+        </Zoom>
         <h5 className="text-light">Computer Engineering Student</h5>
         <CTA />
         <HeaderSocial />
-        
         <div className="me">
           <img src={ME} alt="me" />
         </div>
-
         <a href="#contact" className='scroll__down'>Scroll Down</a>
 
       </div>

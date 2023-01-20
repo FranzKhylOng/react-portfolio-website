d800f0591d945f2a112dc96d60780f6a168c868a
FranzKhyl <117539969+FranzKhylOng@users.noreply.github.com>
2023-01-21 02:21:14 +0800

    Chatbot

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,12 @@ import Experience from './components/experience/Experience'
 import Portfolio from './components/portfolio/Portfolio'
 import Contact from './components/contact/Contact'
 import Footer from './components/footer/Footer'
+import Chat from './components/chat/Chat'
 
-const app = () => {
+const app = () => { 
   return (
     <>
+        <Chat />
         <Header />
         <Nav />
         <About />
